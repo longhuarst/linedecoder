85547c5ea79a723618da5938b8176dc1bdd0762d
coocx <841105197@qq.com>
2018-12-25 09:44:58 +0800

    增加 tty 接口

---
+++ tty.cpp
@@ -0,0 +1,5 @@
+//
+// Created by coocx on 18-12-25.
+//
+
+#include "tty.h"
---
+++ tty.h
@@ -0,0 +1,8 @@
+//
+// Created by coocx on 18-12-25.
+//
+
+#ifndef LINEDECODER_TTY_H
+#define LINEDECODER_TTY_H
+
+#endif //LINEDECODER_TTY_H
