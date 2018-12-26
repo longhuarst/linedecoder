39eecf126a7e20e3852801b18fb99a2fda318aac
coocx <841105197@qq.com>
2018-12-26 08:41:05 +0800

    增加 tty 调用

--- main.cpp
+++ main.cpp
@@ -6,6 +6,8 @@
 #include "clhlib_ringbuffer.h"
 
 
+#include "tty.h"
+
 clhlib_ringbuffer(int)
 
 
@@ -15,6 +17,7 @@ clhlib_ringbuffer(int)
 
 
 
+clh::serial::tty mytty;//串口对象
 
 
 
@@ -25,7 +28,49 @@ clhlib_ringbuffer(int)
 
 
 
-int main() {
+
+int main(int argc, char *argv[]) {
     std::cout << "Hello, World!" << std::endl;
+
+    std::string devpath_ = "/dev/ttyUSB0";
+
+
+    if (argc > 2){
+        for (int i=1;i<argc;++i){
+            if (std::string(argv[i]) == "-D" || std::string(argv[i]) == "-d"){
+                std::cout<<"设备路径 = "<<std::string(argv[i+1])<<std::endl;
+                devpath_ = std::string(argv[i+1]);
+            }
+        }
+    }
+
+
+    //打开串口
+    mytty.init(devpath_,115200);
+
+
+
+
+
+
+
+    if (mytty.getfd() < 0){
+        //串口没有被正常打开
+    }else{
+        mytty.closefd();
+    }
+
+
+
+
+
+
+
+
     return 0;
 }
+
+
+
+
+
--- tty.h
+++ tty.h
@@ -5,4 +5,74 @@
 #ifndef LINEDECODER_TTY_H
 #define LINEDECODER_TTY_H
 
+
+
+#include <iostream>
+#include <map>
+
+
+namespace clh {
+
+
+    namespace serial {
+
+
+
+        class tty
+        {
+        public:
+            tty();
+            virtual ~tty();
+
+
+            int init(std::string filepath, int speed, int databits, int stopbits, int parity);
+            int init(std::string filepath, const char *format = "", ...);
+            //int init(map<std::string, std::string> paramMap);
+            int init(std::string filepath);
+            int init(std::string filepath, int speed);
+
+            int setSpeed(int speed);
+            int setParity(int parity);
+            int setDatabits(int databits);
+            int setStopbits(int stopbits);
+            int openfd(std::string filepath);
+            int closefd();
+
+            int getfd();
+            int getBaudrate();
+            int getDatabits();
+            int getParity();
+            int getStopbits();
+        protected:
+        private:
+
+            //int _open();
+
+            std::string path;
+            int fd;
+            int baudrate;
+            int databits;
+            int parity;
+            int stopbits;
+
+
+        };
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+}
+
+
 #endif //LINEDECODER_TTY_H
